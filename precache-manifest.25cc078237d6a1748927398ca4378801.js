acae0c70242a05c131a745b924e61e1c279d7cb0
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-08-01 18:11:26 -0400

    Updates

---
+++ precache-manifest.25cc078237d6a1748927398ca4378801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb6d13b57750b1893288d776764e8ff",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "11b271611c9c1feec1e7",
+    "url": "/Calculator/static/css/main.6ac97662.chunk.css"
+  },
+  {
+    "revision": "aac5711dcf78aa5fa10a",
+    "url": "/Calculator/static/js/2.22261ae6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Calculator/static/js/2.22261ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b271611c9c1feec1e7",
+    "url": "/Calculator/static/js/main.58c1b3ae.chunk.js"
+  },
+  {
+    "revision": "a0843c05571dcd8658af",
+    "url": "/Calculator/static/js/runtime-main.939bd12a.js"
+  }
+]);
